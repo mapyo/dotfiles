e0f71b17ca9c02d3ed7f27d53efbffa8975acc4d
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-11-13 09:55:46 +0900

    .zshを追加

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://github.com/yuroyoro/dotfiles/blob/master/setup.sh
 
-DOT_FILES=( .zshrc .gitconfig .tmux.conf .tmux .vimrc .vim)
+DOT_FILES=( .zshrc .zsh .gitconfig .tmux.conf .tmux .vimrc .vim)
 
 for file in ${DOT_FILES[@]}
 do
