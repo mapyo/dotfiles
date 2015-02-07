689972b5a078db1792a261a9ee810d5cd87f353a
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2015-02-08 07:51:44 +0900

    ideavimrcの設定も追加

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://github.com/yuroyoro/dotfiles/blob/master/setup.sh
 
-DOT_FILES=( .zshrc .zshrc.local .zsh .gitconfig .tmux.conf .tmux .vimrc .vim)
+DOT_FILES=( .zshrc .zshrc.local .zsh .gitconfig .tmux.conf .tmux .vimrc .vim .ideavimrc)
 
 for file in ${DOT_FILES[@]}
 do
